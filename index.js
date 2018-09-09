2ef9229de2f5aeb98b52dbd0c6c794eef7b415fd
saneergera <saneergera@gmail.com>
2018-09-10 02:05:38 +0530

    added in db support

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express = require('express');
-require('./services/passport.js');
-const mongoose = require('mongoose');
-const keys = require('./config/keys.js');
-require('./models/user.js');
+const express = require("express");
+
+const mongoose = require("mongoose");
+const keys = require("./config/keys.js");
+require("./models/user.js");
+require("./services/passport.js");
 const app = express();
-mongoose.connect('keys.mongooseURI');
-require('./routes/auth_routes.js')(app);
 
-
+mongoose.connect(keys.mongooseURI);
+require("./routes/auth_routes.js")(app);
 
 const PORT = process.env.PORT || 5000;
 
--- services/passport.js
+++ services/passport.js
@@ -1,20 +1,19 @@
-const passport = require('passport');
-const googleAuth = require('passport-google-oauth20').Strategy;
-const keys = require('../config/keys.js');
+const passport = require("passport");
+const googleAuth = require("passport-google-oauth20").Strategy;
+const keys = require("../config/keys.js");
+const mongoose = require("mongoose");
 
+const User = mongoose.model("users");
 
-passport.use( new googleAuth({
-
+passport.use(
+  new googleAuth(
+    {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
-
-    }, (accessToken,refreshToken,profile,done) => {
-
-      console.log("accessToken:",accessToken);
-      console.log("refreshToken",refreshToken);
-      console.log("profile",profile);
-      console.log("done",done)
-
-
-    }));
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      new User({ googleId: profile.id }).save();
+    }
+  )
+);
