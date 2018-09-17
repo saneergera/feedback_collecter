05a7bb33c6a1365345203fd9ce499f119ce4a51e
saneergera <saneergera@gmail.com>
2018-09-17 16:49:53 +0530

    deployment error resolved

--- index.js
+++ index.js
@@ -29,6 +29,8 @@ require("./routes/payments")(app);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("./client/build"));
 
+  const path = require("path");
+
   app.use("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
