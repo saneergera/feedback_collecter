e3be21bbf9487c7245983ee669c043044ce2d9f7
saneergera <saneergera@gmail.com>
2018-09-17 23:46:59 +0530

    deployment error resolved

--- config/prod.js
+++ config/prod.js
@@ -1,15 +1,15 @@
 module.exports = {
-  // googleClientID: process.env.GOOGLE_CLIENT_ID,
-  // googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  // mongooseURI: process.env.MANGOOSEURI,
-  // cookieKey: process.env.COOKIEKEY,
-  // publishKey: process.env.PUBLISHKEY,
-  // secretKey: process.env.SECRETKEY
-  googleClientID:
-    "278572966670-72jecmkk024og8si8d684uei4bgk2m35.apps.googleusercontent.com",
-  googleClientSecret: "tQcWFirq9Y8-9DyNe-2ST3Gw",
-  mongooseURI: "mongodb://root:saneer123@ds251902.mlab.com:51902/emily",
-  cookieKey: "abcderfg",
-  publishKey: "pk_test_g53LrX8vUsoNvoR8Wwq6AVy5",
-  secretKey: "sk_test_p8wd0tYf8YK4KN6329ghmMjl"
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongooseURI: process.env.MANGOOSEURI,
+  cookieKey: process.env.COOKIEKEY,
+  publishKey: process.env.PUBLISHKEY,
+  secretKey: process.env.SECRETKEY
+  // googleClientID:
+  //   "278572966670-72jecmkk024og8si8d684uei4bgk2m35.apps.googleusercontent.com",
+  // googleClientSecret: "tQcWFirq9Y8-9DyNe-2ST3Gw",
+  // mongooseURI: "mongodb://root:saneer123@ds251902.mlab.com:51902/emily",
+  // cookieKey: "abcderfg",
+  // publishKey: "pk_test_g53LrX8vUsoNvoR8Wwq6AVy5",
+  // secretKey: "sk_test_p8wd0tYf8YK4KN6329ghmMjl"
 };
