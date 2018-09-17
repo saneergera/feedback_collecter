1e1f9c09dc11a00e55ab17ddc120e6866ebf2185
saneergera <saneergera@gmail.com>
2018-09-17 23:15:50 +0530

    deployment error resolved

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: process.env.GOOGLE_CLIENT_SECRETTTTT,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongooseURI: process.env.MANGOOSEURI,
   cookieKey: process.env.COOKIEKEY,
   publishKey: process.env.PUBLISHKEY,
